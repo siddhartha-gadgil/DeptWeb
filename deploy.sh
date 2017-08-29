db33010fbee155d405cd244f25b9b82c2c45ac73
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2017-08-29 14:09:47 +0530

    improved deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 git pull
+target=${2:-~/public}
 bundle exec jekyll build --config _config.yml,_config-root.yml --destination ./_root-site
-rsync -avz ./_root-site/ $1:/home/gadgil/public/
+rsync -avz ./_root-site/ $1:$target/
