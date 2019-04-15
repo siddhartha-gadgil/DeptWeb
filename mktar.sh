11ed06623ddf2a643f48e6262daee52d6a201e5b
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-04-15 15:34:52 +0530

    tar generation incrementally

--- mktar.sh
+++ mktar.sh
@@ -2,6 +2,6 @@
 set -e
 cd /home/gadgil/code/DeptWeb
 git pull
-/usr/local/bin/bundle exec jekyll build --config _config.yml,_config-root.yml --destination ./_root-site
+bundle exec jekyll build --incremental --config _config.yml,_config-root.yml --destination ./_root-site
 mkdir -p tarball
 tar -czf tarball/dept.tgz _root-site
