299fe0f8a56285950b04d1b87b79a69b7364c744
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-04-26 10:09:54 +0530

    deploy modified

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@ set -e
 git pull
 host=${1:-"root@math.iisc.ac.in"}
 target=${2:-~/var/www/html}
-echo $host
 bundle exec jekyll build --config _config.yml,_config-root.yml --destination ./_root-site
 rsync -avz ./_root-site/ $host:$target/
