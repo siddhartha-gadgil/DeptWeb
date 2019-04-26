57edf2e514f63c3f3c063f34dc4e524495496cfd
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-04-26 08:26:41 +0530

    changed deployment defaults

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 git pull
-target=${2:-~/public}
+host=${1:-"root@math.iisc.ac.in"}
+target=${2:-~/var/www/html}
+echo $host
 bundle exec jekyll build --config _config.yml,_config-root.yml --destination ./_root-site
-rsync -avz ./_root-site/ $1:$target/
+rsync -avz ./_root-site/ $host:$target/
