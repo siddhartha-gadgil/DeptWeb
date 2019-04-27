83344bd7cdb5ff9f28372f763d2373e994004034
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-04-27 11:05:20 +0530

    improved deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ set -e
 git pull
 host=${1:-"root@math.iisc.ac.in"}
 target=${2:-~/var/www/html}
+rm -rf _root-site || true
 bundle exec jekyll build --config _config.yml,_config-root.yml --destination ./_root-site
 rsync -avz ./_root-site/ $host:$target/
--- server/src/Server.scala
+++ server/src/Server.scala
@@ -1,6 +1,8 @@
 package server
 
-object Server extends cask.MainRoutes{
+import scala.util.Try
+
+object ServerRoutes extends cask.Routes{
   @cask.get("/")
   def hello() = {
     "Hello World"
@@ -10,11 +12,16 @@ object Server extends cask.MainRoutes{
 
   def mkTar() = os.proc("bash", "mktar.sh").call()
 
-  @cask.get("/dept.tgz")
+  @cask.post("/dept.tgz")
   def tarBall(request: cask.Request) = {
     mkTar()
     os.read(tarfile)
   }
 
   initialize()
+}
+
+object Server extends cask.Main(ServerRoutes){
+  override def port = 6060
+  override def host = Try(sys.env("IP")).getOrElse("localhost")
 }
